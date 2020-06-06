425d0788a7f8e9dca6daf5f3130d5753f161b228
Mayank Pabbi <mayankpabbi@gmail.com>
2020-06-06 20:25:57 +0530

    Updates

---
+++ precache-manifest.fd4d40317be197cba5b4af3d925db2fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec846c8106731f53c43b21151862cf3",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "121c2c64b40a8f077c43",
+    "url": "/pokemon/static/css/main.29afa99b.chunk.css"
+  },
+  {
+    "revision": "a41335c963ddefa63c94",
+    "url": "/pokemon/static/js/2.72a9ac95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.72a9ac95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "121c2c64b40a8f077c43",
+    "url": "/pokemon/static/js/main.9ab54c3e.chunk.js"
+  },
+  {
+    "revision": "e1366ef5cfb767bc3a5a",
+    "url": "/pokemon/static/js/runtime-main.2bfe477e.js"
+  }
+]);
